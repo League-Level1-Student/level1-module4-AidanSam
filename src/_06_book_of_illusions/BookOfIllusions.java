8063b56f36af8f09fa1146334598ab53bf938b88
Aidan <Aidan@AidanPC.lan>
2021-02-10 19:50:31 -0800

    g

--- src/_06_book_of_illusions/BookOfIllusions.java
+++ src/_06_book_of_illusions/BookOfIllusions.java
@@ -31,14 +31,21 @@ public class BookOfIllusions extends MouseAdapter {
 	String cup = "cup.png";
 
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
+	JLabel label;
+
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+	label = loadImageFromComputer(cup);
 		// 8. add your JLabel to the frame
+	frame.add(label);
 		// 9. call the pack() method on the frame
+	frame.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
+	
 	}
 
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
+		System.out.println("clicked!");
 		// 12. remove everything from the frame that was added earlier
 		// 13. load a new image like before (this is more than one line of code)
 		// 14. pack the frame
