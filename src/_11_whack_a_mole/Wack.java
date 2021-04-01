46e07238153d99330f5fdeeb07f3e7c2372b29a7
Aidan <Aidan@AidanPC.lan>
2021-03-31 19:52:40 -0700

    4ee

--- src/_11_whack_a_mole/Wack.java
+++ src/_11_whack_a_mole/Wack.java
@@ -1,5 +1,28 @@
 package _11_whack_a_mole;
 
-public class Wack {
+import java.awt.event.ActionEvent;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Wack {
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+     JButton button1 = new JButton();
+     JButton button2 = new JButton();
+     JButton button3 = new JButton();
+     JButton button4 = new JButton();
+public void run() {
+
+	frame.setVisible(true);
+	frame.setSize(1000, 1000);
+	frame.add(button1);
+	frame.add(button2);
+	frame.add(button3);
+	frame.add(button4);
+	
+}
 }
--- src/_11_whack_a_mole/WackRunner.java
+++ src/_11_whack_a_mole/WackRunner.java
@@ -1,6 +1,6 @@
 package _11_whack_a_mole;
 
-import _10_pig_latin.PigLatin;
+import javax.swing.JFrame;
 
 public class WackRunner {
 	public static void main(String[] args) {
