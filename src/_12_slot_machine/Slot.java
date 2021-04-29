04c8d64deb35efe0dbe5020e3442490e6578c9b5
Aidan <Aidan@AidanPC.lan>
2021-04-28 19:59:21 -0700

    qwr

--- src/_12_slot_machine/Slot.java
+++ src/_12_slot_machine/Slot.java
@@ -9,36 +9,67 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Slot {
 	int count = 0;
 	Frame frame = new JFrame();
+	JPanel panel = new JPanel();
 	Random ran = new Random();
 	Random rand = new Random();
-	JPanel panel = new JPanel();
+	Random rando = new Random();
 	public void run() {
+		frame.setVisible(true);
 		panel = new JPanel();
-		for(int i = 0; i<3; i++) {
-	int num = ran.nextInt(24);
+		frame.add(panel);
+	int num = ran.nextInt(4);
+	int numb = rand.nextInt(4);
+	int numbe = rando.nextInt(4);
+	JLabel label1 = new JLabel();
+	JLabel label2 = new JLabel();
+	JLabel label3 = new JLabel();
+	panel.add(label1);
+	panel.add(label2);
+	panel.add(label3);
+	
 if(num==1) {
-	
+label1.setIcon(createLabelImage("Untitled.png"));
 }else if(num==2) {
-	
+label1.setIcon(createLabelImage("Untitled2.png"));
 }else if(num==3) {
-	
+label1.setIcon(createLabelImage("Untitled3.png"));
 }
+if(numb==1) {
+label2.setIcon(createLabelImage("Untitled.png"));
+}else if(numb==2) {
+label2.setIcon(createLabelImage("Untitled2.png"));
+}else if(numb==3) {
+label2.setIcon(createLabelImage("Untitled3.png"));
 }
+if(numbe==1) {
+label3.setIcon(createLabelImage("Untitled.png"));
+}else if(numbe==2) {
+label3.setIcon(createLabelImage("Untitled2.png"));
+}else if(numbe==3) {
+label3.setIcon(createLabelImage("Untitled3.png"));
+}
+frame.pack();
+if(num==numb) {
+	if(numb==numbe) {
+		JOptionPane.showMessageDialog(null, "You win");
 	}
-	private JLabel createLabelImage(String fileName) throws MalformedURLException{
+}
+}
+	private Icon createLabelImage(String fileName){
         URL imageURL = getClass().getResource(fileName);
 	if (imageURL == null){
 		System.err.println("Could not find image " + fileName);
-		return new JLabel();
+		return null;
 	}
 	Icon icon = new ImageIcon(imageURL);
 	JLabel imageLabel = new JLabel(icon);
-	return imageLabel;
+	return icon;
 
 	}
 	
--- src/_12_slot_machine/Slot_Runner.java
+++ src/_12_slot_machine/Slot_Runner.java
@@ -2,7 +2,8 @@ package _12_slot_machine;
 
 
 public class Slot_Runner {
-public static void main(String[] args) {
+	public static void main(String[] args) {
+		
 		
 		new Slot().run();
 }
